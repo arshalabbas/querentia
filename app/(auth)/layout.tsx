d8c73c5d46a6f8c5c2a874c78aa4e3e6589306e7
arshalabbas <arshalabbas123@gmail.com>
2024-01-29 00:05:50 +0530

    minor changes on clerk components

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,4 +1,5 @@
 import { ClerkProvider } from "@clerk/nextjs";
+import "@/app/globals.css";
 
 import { Inter } from "next/font/google";
 
@@ -10,11 +11,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        variables: { colorPrimary: "#30a7be" },
+        elements: {
+          formButtonPrimary: "btn btn-info",
+          socialButtonsBlockButton:
+            "bg-white border-gray-200 hover:bg-transparent hover:border-info text-gray-600 hover:text-black",
+          socialButtonsBlockButtonText: "font-semibold",
+          card: "bg-base-200",
+        },
+      }}
+    >
       <html lang="en">
-        <body className={`${inter.className} min-h-screen flex flex-col`}>
-          {children}
-        </body>
+        <body className={`${inter.className} bg-base-100`}>{children}</body>
       </html>
     </ClerkProvider>
   );
